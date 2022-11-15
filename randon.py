ce02017ae44f7e4307621b58905f5cab58a8c188
Israel Adegoke <117425865+ezzisrael@users.noreply.github.com>
2022-11-15 20:15:07 +0100

    Update randon.py

--- randon.py
+++ randon.py
@@ -18,47 +18,41 @@
 import random
 
  def guess (x):
-     random_number = random.randint(1,x)
-     guess = 0
-     while guess != random_number:
-         guess = int(input(f'Guess a number between 1 and {x}: '))
-         if guess < random_number:
-             print('Sorry guess again. Too low.')
-         elif guess > random_number:
-             print('Sorry guess again. Too high.')
-
-     print('Yay, Congrants!. You have guessed the number.')
-
-
-
- num = int (input (“Enter any number to test whether it is odd or even: “)
-
- if (num % 2) == 0:
-     print (“The number is even”)
- else:
-     print (“The provided number is odd”)
-
-
-
-
-
-
-def guess (x):
     random_number = random.randint(1,x)
     guess = 0
     while guess != random_number:
-        guess = int(input(f'Enter number guessed between 1 and {x}: '))
+        guess = int(input(f'Guess a between 1 and {x}: '))
 
-        if (x % 2) == 0 and guess < random_number:
-            print('Sorry try again: Guess is an even number but too low')
-        elif (x % 2) == 0 and guess > random_number:
-            print('Sorry guess again. number guessed is even and too high')
-        elif (x % 3) == 0 and guess > random_number:
-            print('Sorry guess again. number guessed is odd and too high')
-        elif (x % 3) == 0 and guess < random_number:
-            print('Sorry try again: Guess is an odd number and too low')
+        if guess%2 == 0 and guess < random_number:
+            print(f'{guess} the guessed number is EVEN and LOW')
+        elif guess%2 != 0 and guess > random_number:
+            print(f'try again {guess} is ODD and HIGH')
+        elif guess%2 == 0 and guess > random_number:
+            print(f'Try again {guess} is EVEN and High')
+        elif guess%2 != 0 and guess < random_number:
+            print(f'Try again {guess} is ODD number and LOW')
 
-    print(f'Weldone you have guessed right {random_number} correctly')
+    print(f'Guessed number is: {random_number}')
 
-guess()
 
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        
+        else:
+             guess = low
+             
+        feedback = input(f' Is {guess} too high (H), too low (L), or correct(c)?? ').lower
+        if feedback == 'h':
+            high = guess -1
+        elif feedback == 'l':
+            low = guess + 1
+
+print(f' computer guess number {guess} correctly! ')
+
+
+guess(10)
